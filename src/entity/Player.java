6532d40ce6010f35c261a35c49488fdfe60bdde5
BARLOY Nathan <nathan.barloy@telecomnancy.eu>
2017-11-24 23:53:18 +0100

    changement controles pour faire comme dans isaac !

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -15,6 +15,7 @@ public class Player extends Entity {
 	private boolean right;
 	private boolean left;
 	private boolean rightLeft;
+	private double speed;
 
 	public Player() throws SlickException{
 		World.player = this;
@@ -22,6 +23,7 @@ public class Player extends Entity {
 		hitbox = new Rectangle(x,y,width,height);
 		width=36;
 		height=36;
+		speed=0.25;
 	}
 	
 	@Override
@@ -51,24 +53,32 @@ public class Player extends Entity {
 		}
 	}
 	
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
 	public void keyPressed(int key, char c) {
 		switch (key){
 
-		case Input.KEY_UP:
+		case Input.KEY_Z:
 			up = true;
 			updown=false;
 			break;
 
-		case Input.KEY_DOWN:
+		case Input.KEY_S:
 			down=true;
 			updown=true;
 			break;
 
-		case Input.KEY_LEFT:
+		case Input.KEY_Q:
 			left=true;
 			rightLeft=false;
 			break;
-		case Input.KEY_RIGHT:
+		case Input.KEY_D:
 			right=true;
 			rightLeft=true;
 			break;
@@ -81,16 +91,16 @@ public class Player extends Entity {
 
 	public void keyReleased(int key, char c) {
 		switch (key) {
-		case Input.KEY_UP:
+		case Input.KEY_Z:
 			up=false;
 			break;
-		case Input.KEY_RIGHT:
+		case Input.KEY_D:
 			right=false;
 			break;
-		case Input.KEY_LEFT:
+		case Input.KEY_Q:
 			left=false;
 			break;
-		case Input.KEY_DOWN:
+		case Input.KEY_S:
 			down=false;
 			break;
 		}
@@ -100,16 +110,20 @@ public class Player extends Entity {
 		speedX = 0;
 		speedY = 0;
 		if((up && !down) || (up && down && !updown)) {
-			speedY=-0.2;
+			speedY=-speed;
 		}
 		if((down && !up) || (up && down && updown)) {
-				speedY=0.2;
+				speedY=speed;
 		}
 		if((left && !right)|| (left && right && !rightLeft)) {
-			speedX = -0.2;
+			speedX = -speed;
 		}
 		if((!left && right)|| (left && right && rightLeft)) {
-				speedX = 0.2;
+				speedX = speed;
+		}
+		if (speedX!=0 && speedY!=0) {
+			speedX/=Math.sqrt(2);
+			speedY/=Math.sqrt(2);
 		}
 		
 		x+=dt*speedX;
