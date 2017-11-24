eb435a2417d3847ffc9f3258b04e479a7f416f16
canalarthur <canalarthur@yahoo.fr>
2017-11-24 20:37:08 +0100

    Deplacement de la classe projectile

--- src/entity/Projectile.java
+++ src/entity/Projectile.java
@@ -3,4 +3,16 @@ package entity;
 
 public class Projectile extends Entity{
 
+	@Override
+	public void die() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void checkForCollision() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
