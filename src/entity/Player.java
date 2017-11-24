f509520a4d9dfe17107ac1a77da255e921f2be26
BARLOY Nathan <nathan.barloy@telecomnancy.eu>
2017-11-25 00:31:48 +0100

    le perso il tourne

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,9 +1,12 @@
 package entity;
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.state.StateBasedGame;
 
 import general.World;
 
@@ -16,14 +19,30 @@ public class Player extends Entity {
 	private boolean left;
 	private boolean rightLeft;
 	private double speed;
+	private int direction;
+	private Image spriteU;
+	private Image spriteR;
+	private Image spriteD;
+	private Image spriteL;
+	private boolean nord;
+	private boolean est;
+	private boolean sud;
+	private boolean ouest;
+	private boolean nordsud;
+	private boolean estouest;
+	
 
 	public Player() throws SlickException{
 		World.player = this;
-		sprite = new Image(World.DIRECTORY_IMAGES+"player.png");
+		spriteU = new Image(World.DIRECTORY_IMAGES+"playerHaut.png");
+		spriteR = new Image(World.DIRECTORY_IMAGES+"playerDroite.png");
+		spriteD = new Image(World.DIRECTORY_IMAGES+"playerBas.png");
+		spriteL = new Image(World.DIRECTORY_IMAGES+"playerGauche.png");
 		hitbox = new Rectangle(x,y,width,height);
 		width=36;
 		height=36;
 		speed=0.25;
+		direction=2;
 	}
 	
 	@Override
@@ -82,9 +101,30 @@ public class Player extends Entity {
 			right=true;
 			rightLeft=true;
 			break;
+			
+			
+		case Input.KEY_UP:
+			nord = true;
+			nordsud=false;
+			break;
+
+		case Input.KEY_DOWN:
+			sud=true;
+			nordsud=true;
+			break;
+
+		case Input.KEY_LEFT:
+			ouest=true;
+			estouest=false;
+			break;
+		case Input.KEY_RIGHT:
+			est=true;
+			estouest=true;
+			break;
 		case Input.KEY_ESCAPE:
 			System.exit(0);
 			break;
+
 		}
 
 	}
@@ -102,6 +142,19 @@ public class Player extends Entity {
 			break;
 		case Input.KEY_S:
 			down=false;
+			break;
+		
+		case Input.KEY_UP:
+			nord=false;
+			break;
+		case Input.KEY_RIGHT:
+			est=false;
+			break;
+		case Input.KEY_LEFT:
+			ouest=false;
+			break;
+		case Input.KEY_DOWN:
+			sud=false;
 			break;
 		}
 	}
@@ -130,4 +183,30 @@ public class Player extends Entity {
 		y+=dt*speedY;
 	}
 	
+	public void setDir() {
+		direction=2;
+		
+		if((nord && !sud) || (nord && sud && !nordsud)) {
+			direction=0;
+		}
+		if((sud && !nord) || (nord && sud && nordsud)) {
+			direction=2;
+		}
+		if((ouest && !est)|| (ouest && est && !estouest)) {
+			direction=3;
+		}
+		if((!ouest && est)|| (ouest && est && estouest)) {
+			direction=1;
+		}
+		
+	}
+	
+	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+		setDir();
+		if (direction==0) g.drawImage(spriteU,(float) x,(float) y);
+		if (direction==1) g.drawImage(spriteR,(float) x,(float) y);
+		if (direction==2) g.drawImage(spriteD,(float) x,(float) y);
+		if (direction==3) g.drawImage(spriteL,(float) x,(float) y);
+		
+	}
 }
