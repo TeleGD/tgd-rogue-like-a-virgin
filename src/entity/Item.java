399fbd9416fc880b100e92d477cf09ea3776d765
Antoine Sochala <antoine.sochala@telecomnancy.net>
2017-11-25 05:32:30 +0100

    j'ai fait des trucs sur la monnaie du jeu appelé COIN

--- src/entity/Item.java
+++ src/entity/Item.java
@@ -10,7 +10,9 @@ import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.StateBasedGame;
 
 import general.World;
+import map.Case;
 import map.CaseVide;
+import map.Mur;
 
 
 public class Item extends Entity {
@@ -20,32 +22,57 @@ public class Item extends Entity {
 	public Item() throws SlickException{
 		World.item = this;
 		Random r = new Random();
-		switch (r.nextInt(5)) {
+		switch (r.nextInt(8)) {
         case 0:  
         	type = "SpeedUp";
         	sprite = new Image(World.DIRECTORY_IMAGES+"itemSpeedUp.png");
+        	x=342;
+    		y=342;
             break;
         case 1:
         	type = "SpeedDown";
         	sprite = new Image(World.DIRECTORY_IMAGES+"itemSpeedDown.png");
+        	x=342;
+    		y=342;
             break;
         case 2:  
         	type = "HpUp";
         	sprite = new Image(World.DIRECTORY_IMAGES+"itemHpUp.png");
+        	x=342;
+    		y=342;
             break;
 		case 3:  
 			type = "FireRateUp";
         	sprite = new Image(World.DIRECTORY_IMAGES+"itemFireRateUp.png");
+        	x=342;
+    		y=342;
             break;
 		case 4:  
 			type = "FireRateDown";
         	sprite = new Image(World.DIRECTORY_IMAGES+"itemFireRateDown.png");
+        	x=342;
+    		y=342;
             break;
+        default:
+        	type = "Coin";
+        	sprite = new Image(World.DIRECTORY_IMAGES+"itemCoin.png");
+        	Case[][] c = World.map.getCases();
+        	boolean caseNotFound = true;
+        	int posx;
+        	int posy;
+        	while(caseNotFound){
+	    		posx = (r.nextInt(17)+2);
+	    		posy = (r.nextInt(17)+2);
+	        	if(c[posx][posy] instanceof CaseVide){
+	        		x = posx*36;
+	        		y = posy*36;
+	        		caseNotFound = false;
+	        	}
+        	}
+        	break;
 		}
 		width=36;
 		height=36;
-		x=342;
-		y=342;
 		hitbox = new Rectangle(x,y,width,height);
 	}
 	
@@ -62,7 +89,7 @@ public class Item extends Entity {
 	}
 	
 	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-		g.drawImage(sprite,342,342);
+		g.drawImage(sprite,x,y);
 	}
 
 }
